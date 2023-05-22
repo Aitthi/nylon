ee5a54c09ead035312de67c9e3efd6f246609dd0
Aitthi Arsa <meanstack20@gmail.com>
2023-05-22 08:44:58 +0700

    refactor/NylonFactory

--- nylon/core/nylon-factory.ts
+++ nylon/core/nylon-factory.ts
@@ -1,5 +1,5 @@
 import { NylonNode } from '../nylon.node'
-import { Handler, NylonOptions, Request, Response, SafeAny } from '../types'
+import { Handler, MethodMetadata, NylonOptions, Request, SafeAny } from '../types'
 
 export class NylonFactoryStatic {
   private routes: {
@@ -23,46 +23,45 @@ export class NylonFactoryStatic {
     if (!controllers) {
       controllers = []
     }
-    controllers.forEach((class_controller: SafeAny) => {
-      const controller = new class_controller()
-      const basePath = Reflect.getMetadata('path', class_controller) as string
-      let methods = Reflect.getMetadata('methods', controller) as {
-        method: string
-        path: string
-        descriptor: PropertyDescriptor
-        args: {
-          type: 'params' | 'query'
-          value: string
-        }[]
-        response: Response
-      }[]
-      if (!methods) methods = []
-      methods.forEach((method) => {
-        const handlers = [] as Handler[]
-        const handler = async (req: Request) => {
-          const args = [] as SafeAny[]
-          const instance = method.descriptor.value.bind(controller)
-          if (method.args) {
-            method.args.forEach((arg) => {
-              if (arg.type === 'params') {
-                args.push(req.params[arg.value])
-              } else if (arg.type === 'query') {
-                args.push(req.query[arg.value])
-              }
-            })
-          }
-          const rs = await instance(...args)
-          return {
-            ...method.response,
-            body: rs,
-          }
-        }
-        handlers.push(handler)
-        this.routes[`${basePath}${method.path}`] = handlers
-      })
-    })
+    this.registerController(controllers)
     const modules: SafeAny[] = Reflect.getMetadata('modules', module as SafeAny) ?? []
     modules.forEach((class_module: SafeAny) => this.loadModule(class_module))
+  }
+
+  private registerController(controllerCls: SafeAny[]) {
+    controllerCls.forEach((class_controller: SafeAny) => {
+      const controller = new class_controller()
+      const basePath = Reflect.getMetadata('path', class_controller) as string
+      let methods = Reflect.getMetadata('methods', controller) as MethodMetadata[]
+      if (!methods) methods = []
+      this.registerMethod(methods, controller, basePath)
+    })
+  }
+
+  private registerMethod(methods: MethodMetadata[], controller: SafeAny, basePath: string) {
+    methods.forEach((method) => {
+      const handlers = [] as Handler[]
+      const handler = async (req: Request) => {
+        const args = [] as SafeAny[]
+        const instance = method.descriptor.value.bind(controller)
+        if (method.args) {
+          method.args.forEach((arg) => {
+            if (arg.type === 'params') {
+              args.push(req.params[arg.value])
+            } else if (arg.type === 'query') {
+              args.push(req.query[arg.value])
+            }
+          })
+        }
+        const rs = await instance(...args)
+        return {
+          ...method.response,
+          body: rs,
+        }
+      }
+      handlers.push(handler)
+      this.routes[`${basePath}${method.path}`] = handlers
+    })
   }
 
   listen(port: number, host: string, callback: () => void) {
